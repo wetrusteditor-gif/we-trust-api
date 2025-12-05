6f878d6b4e08d5d231ad572ef832761979dd1638
wetrusteditor-gif <wetrusteditor@gmail.com>
2025-12-05 19:10:11 +0530

    Update AppDbContext.cs

--- WeTrust.Api/Data/AppDbContext.cs
+++ WeTrust.Api/Data/AppDbContext.cs
@@ -1,1 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using WeTrust.Api.Models;
+
+namespace WeTrust.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<OrganizationSetting> OrganizationSettings { get; set; }
+        public DbSet<AccountHead> AccountHeads { get; set; }
+        public DbSet<DocumentSequence> DocumentSequences { get; set; }
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<JournalLine> JournalLines { get; set; }
+    }
+}
 
