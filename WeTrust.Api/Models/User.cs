277dd4f4db8eefed212f159414b517463a72da86
wetrusteditor-gif <wetrusteditor@gmail.com>
2025-12-05 19:10:36 +0530

    Update User.cs

--- WeTrust.Api/Models/User.cs
+++ WeTrust.Api/Models/User.cs
@@ -1,1 +1,13 @@
+namespace WeTrust.Api.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
 
