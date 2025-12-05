6ff271f40abc80a5a51af96561dcec64bed93a2e
wetrusteditor-gif <wetrusteditor@gmail.com>
2025-12-05 21:40:18 +0530

    Debug: detailed error output for /account-heads

--- WeTrust.Api/Controllers/AccountHeadsController.cs
+++ WeTrust.Api/Controllers/AccountHeadsController.cs
@@ -12,15 +12,36 @@ namespace WeTrust.Api.Controllers
         public AccountHeadsController(AppDbContext db) { _db = db; }
 
         [HttpGet]
-        public IActionResult GetAll() => Ok(_db.AccountHeads.Where(a => a.IsActive).ToList());
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var list = _db.AccountHeads.Where(a => a.IsActive).ToList();
+                return Ok(list);
+            }
+            catch (Exception ex)
+            {
+                // Log to console (Render will capture it)
+                Console.Error.WriteLine("Exception in /account-heads: " + ex.ToString());
+                // Return the exception detail temporarily for debugging (remove later)
+                return Problem(title: "exception in account-heads", detail: ex.ToString());
+            }
+        }
 
         [HttpPost]
         public IActionResult Create(AccountHead a)
         {
-            _db.AccountHeads.Add(a);
-            _db.SaveChanges();
-            return Ok(a);
+            try
+            {
+                _db.AccountHeads.Add(a);
+                _db.SaveChanges();
+                return Ok(a);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Exception in POST /account-heads: " + ex.ToString());
+                return Problem(title: "exception creating account-head", detail: ex.ToString());
+            }
         }
     }
 }
-
