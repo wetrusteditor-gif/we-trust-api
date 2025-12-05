1ffaeb077a6b0c5808be199c584218bdffb63708
wetrusteditor-gif <wetrusteditor@gmail.com>
2025-12-05 19:12:18 +0530

    Update JournalLine.cs

--- WeTrust.Api/Models/JournalLine.cs
+++ WeTrust.Api/Models/JournalLine.cs
@@ -1,1 +1,13 @@
+namespace WeTrust.Api.Models
+{
+    public class JournalLine
+    {
+        public int LineId { get; set; }
+        public int DocumentId { get; set; }
+        public int AccountHeadId { get; set; }
+        public decimal Debit { get; set; }
+        public decimal Credit { get; set; }
+        public string? LineNarration { get; set; }
+    }
+}
 
