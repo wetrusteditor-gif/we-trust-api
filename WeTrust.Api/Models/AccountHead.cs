3c466b0abc79d7096c25c226ed1de5f42cac9dce
wetrusteditor-gif <wetrusteditor@gmail.com>
2025-12-05 19:11:23 +0530

    Update AccountHead.cs

--- WeTrust.Api/Models/AccountHead.cs
+++ WeTrust.Api/Models/AccountHead.cs
@@ -1,1 +1,13 @@
+namespace WeTrust.Api.Models
+{
+    public class AccountHead
+    {
+        public int AccountHeadId { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public bool IsActive { get; set; } = true;
+        public decimal OpeningBalance { get; set; }
+        public char OpeningBalanceType { get; set; } = 'D';
+    }
+}
 
