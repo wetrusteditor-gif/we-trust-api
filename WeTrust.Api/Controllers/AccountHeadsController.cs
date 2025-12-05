7afe3bb48e4bbe2ad8d11d6e090995b3d678890a
wetrusteditor-gif <wetrusteditor@gmail.com>
2025-12-05 19:13:44 +0530

    Update AccountHeadsController.cs

--- WeTrust.Api/Controllers/AccountHeadsController.cs
+++ WeTrust.Api/Controllers/AccountHeadsController.cs
@@ -1,1 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using WeTrust.Api.Data;
+using WeTrust.Api.Models;
+
+namespace WeTrust.Api.Controllers
+{
+    [ApiController]
+    [Route("account-heads")]
+    public class AccountHeadsController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        public AccountHeadsController(AppDbContext db) { _db = db; }
+
+        [HttpGet]
+        public IActionResult GetAll() => Ok(_db.AccountHeads.Where(a => a.IsActive).ToList());
+
+        [HttpPost]
+        public IActionResult Create(AccountHead a)
+        {
+            _db.AccountHeads.Add(a);
+            _db.SaveChanges();
+            return Ok(a);
+        }
+    }
+}
 
