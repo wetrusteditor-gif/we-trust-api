b073ff0015f8168f7e41ec9ab59b2d5b74077994
wetrusteditor-gif <wetrusteditor@gmail.com>
2025-12-05 19:11:43 +0530

    Update DocumentSequence.cs

--- WeTrust.Api/Models/DocumentSequence.cs
+++ WeTrust.Api/Models/DocumentSequence.cs
@@ -1,1 +1,11 @@
+namespace WeTrust.Api.Models
+{
+    public class DocumentSequence
+    {
+        public int Id { get; set; }
+        public string DocType { get; set; }
+        public int Year { get; set; }
+        public int CurrentNumber { get; set; }
+    }
+}
 
