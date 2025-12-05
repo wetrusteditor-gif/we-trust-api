605e64662f5cd23d619a4c851ff9040abb2c475f
wetrusteditor-gif <wetrusteditor@gmail.com>
2025-12-05 20:09:08 +0530

    Update AppDbContext.cs

--- WeTrust.Api/Data/AppDbContext.cs
+++ WeTrust.Api/Data/AppDbContext.cs
@@ -13,6 +13,32 @@ namespace WeTrust.Api.Data
         public DbSet<DocumentSequence> DocumentSequences { get; set; }
         public DbSet<Document> Documents { get; set; }
         public DbSet<JournalLine> JournalLines { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Define primary keys explicitly
+            modelBuilder.Entity<User>().HasKey(u => u.UserId);
+            modelBuilder.Entity<OrganizationSetting>().HasKey(o => o.OrgId);
+            modelBuilder.Entity<AccountHead>().HasKey(a => a.AccountHeadId);
+            modelBuilder.Entity<DocumentSequence>().HasKey(ds => ds.Id);
+            modelBuilder.Entity<Document>().HasKey(d => d.DocumentId);
+            modelBuilder.Entity<JournalLine>().HasKey(j => j.LineId);
+
+            // Configure relationships (optional but helpful)
+            modelBuilder.Entity<Document>()
+                .HasMany(d => d.JournalLines)
+                .WithOne()
+                .HasForeignKey(j => j.DocumentId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Configure column lengths / types if desired
+            modelBuilder.Entity<User>().Property(u => u.Username).HasMaxLength(50).IsRequired();
+            modelBuilder.Entity<User>().Property(u => u.PasswordHash).HasMaxLength(255).IsRequired();
+
+            modelBuilder.Entity<AccountHead>().Property(a => a.Name).HasMaxLength(100).IsRequired();
+            modelBuilder.Entity<OrganizationSetting>().Property(o => o.OrgName).HasMaxLength(150).IsRequired();
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
-
