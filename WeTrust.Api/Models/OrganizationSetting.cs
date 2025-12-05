96c61841fba20fad72c5b58e77b46477ac6a41c5
wetrusteditor-gif <wetrusteditor@gmail.com>
2025-12-05 19:11:03 +0530

    Update OrganizationSetting.cs

--- WeTrust.Api/Models/OrganizationSetting.cs
+++ WeTrust.Api/Models/OrganizationSetting.cs
@@ -1,1 +1,17 @@
+namespace WeTrust.Api.Models
+{
+    public class OrganizationSetting
+    {
+        public int OrgId { get; set; }
+        public string OrgName { get; set; }
+        public string? AddressLine1 { get; set; }
+        public string? AddressLine2 { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Pincode { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? LogoUrl { get; set; }
+    }
+}
 
